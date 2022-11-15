ba439b2d0b17c2ca03a6f5426f9d33dc69494faf
mattpettenato <mattpettenato@gmail.com>
2022-11-14 16:59:57 -0800

    first migration

---
+++ db/migrate/20221115005409_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.string :image_url
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
